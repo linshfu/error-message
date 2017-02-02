ef6aa42dbb4fef7cbc7bb1c3f8dcee86cb3b84b6
sean_lin <sean_lin@mail.chungyo.net>
2017-02-02 14:35:50 +0800

    debug

--- dist/errormessage.js
+++ dist/errormessage.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["EM"] = factory();
+		exports["ErrorMessage"] = factory();
 	else
-		root["EM"] = factory();
+		root["ErrorMessage"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
