7e10a36320e708a8a9030b533ea6d4a56a142a3f
sean_lin <sean_lin@mail.chungyo.net>
2017-02-10 15:06:05 +0800

    remove dependencies

--- dist/errormessage.js
+++ dist/errormessage.js
@@ -1,69 +1,101 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _typeof2 = require('babel-runtime/helpers/typeof');
-
-var _typeof3 = _interopRequireDefault(_typeof2);
-
-var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
-
-var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-
-var _createClass2 = require('babel-runtime/helpers/createClass');
-
-var _createClass3 = _interopRequireDefault(_createClass2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var ErrorMessage = function () {
-  function ErrorMessage() {
-    var error = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-    (0, _classCallCheck3.default)(this, ErrorMessage);
-
-    this.error = {
-      code: typeof error.code === 'number' ? error.code : '#',
-      msg: typeof error.msg === 'string' ? error.msg : 'System Error'
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['module', 'exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(module, exports);
+  } else {
+    var mod = {
+      exports: {}
     };
+    factory(mod, mod.exports);
+    global.ErrorMessage = mod.exports;
+  }
+})(this, function (module, exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+    return typeof obj;
+  } : function (obj) {
+    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+  };
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
   }
 
-  (0, _createClass3.default)(ErrorMessage, [{
-    key: 'toString',
-    value: function toString() {
-      return this.error.msg + '[' + this.error.code + ']';
-    }
-  }, {
-    key: 'code',
-    value: function code() {
-      return this.error.code;
-    }
-  }, {
-    key: 'msg',
-    value: function msg() {
-      return ErrorMessage.middleware.reduce(function (error, fn) {
-        return (0, _typeof3.default)(fn(error)) === 'object' ? fn(error) : error;
-      }, this.error).msg;
-    }
-  }], [{
-    key: 'use',
-    value: function use(fn) {
-      if (!fn || typeof fn !== 'function') {
-        return;
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
       }
-
-      ErrorMessage.middleware = ErrorMessage.middleware.concat(Array.isArray(fn) ? fn : [fn]);
     }
-  }, {
-    key: 'parse',
-    value: function parse(error) {
-      return (typeof error === 'undefined' ? 'undefined' : (0, _typeof3.default)(error)) === 'object' ? new ErrorMessage(error) : new ErrorMessage();
-    }
-  }]);
-  return ErrorMessage;
-}();
 
-ErrorMessage.middleware = [];
-exports.default = ErrorMessage;
-module.exports = exports['default'];
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  var ErrorMessage = function () {
+    function ErrorMessage() {
+      var error = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+      _classCallCheck(this, ErrorMessage);
+
+      this.error = {
+        code: typeof error.code === 'number' ? error.code : '#',
+        msg: typeof error.msg === 'string' ? error.msg : 'System Error'
+      };
+    }
+
+    _createClass(ErrorMessage, [{
+      key: 'toString',
+      value: function toString() {
+        return this.error.msg + '[' + this.error.code + ']';
+      }
+    }, {
+      key: 'code',
+      value: function code() {
+        return this.error.code;
+      }
+    }, {
+      key: 'msg',
+      value: function msg() {
+        return ErrorMessage.middleware.reduce(function (error, fn) {
+          return _typeof(fn(error)) === 'object' ? fn(error) : error;
+        }, this.error).msg;
+      }
+    }], [{
+      key: 'use',
+      value: function use(fn) {
+        if (!fn || typeof fn !== 'function') {
+          return;
+        }
+
+        ErrorMessage.middleware = ErrorMessage.middleware.concat(Array.isArray(fn) ? fn : [fn]);
+      }
+    }, {
+      key: 'parse',
+      value: function parse(error) {
+        return (typeof error === 'undefined' ? 'undefined' : _typeof(error)) === 'object' ? new ErrorMessage(error) : new ErrorMessage();
+      }
+    }]);
+
+    return ErrorMessage;
+  }();
+
+  ErrorMessage.middleware = [];
+  exports.default = ErrorMessage;
+  module.exports = exports['default'];
+});
